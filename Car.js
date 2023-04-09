fd49eb3e445224f40985a7b9c63d3eb2dae54781
harjisirvi <92523298+harjisirvi@users.noreply.github.com>
2023-04-09 18:08:40 +0530

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,15 @@
+cancelAnimationFrame.registerComponent("base",{
+    schema:{
+        radius:{type:"number,default: 150"},
+        height:{type:"number",default:3}
+    },
+init: function() {
+    this.el.setAttribute("geometry",{
+        primitive:"cylinder",
+        radius:this.dat.radius,
+        height:this.data.height
+    });
+    this.el.setAttribute("material",{color:"#1769aa"});
+}
+
+});
